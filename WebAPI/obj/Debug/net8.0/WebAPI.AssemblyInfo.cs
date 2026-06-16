847b517f0c7c95bb8c9d925c9ae644f4e8da2efd
kalaisp <kalaisp9025@gmail.com>
2026-06-16 11:28:09 +0530

    m

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7952f9f2482867ac1245d5d141b6a9354f710c8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f0223ffcc766699b7b9ea14a6e06a925a5b5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
